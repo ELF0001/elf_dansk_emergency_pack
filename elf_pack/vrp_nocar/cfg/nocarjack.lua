bed018eda9ef8d6365b9b3075c4b577a4eed6149
ELF0001 <51233821+ELF0001@users.noreply.github.com>
2019-06-26 04:42:25 +0200

    Add files via upload

---
+++ elf_pack/vrp_nocar/cfg/nocarjack.lua
@@ -0,0 +1,33 @@
+cfg = {}
+
+cfg.chance = 95 -- chance of being unlocked in percentage
+
+cfg.blacklist = { -- vehicles that will always be locked when spawned naturally
+  "T20",
+  "police",
+  "police2",
+  "police3",
+  "police4",
+  "police5",
+  "police6",
+  "police7",
+  "police8",
+  "policet",
+  "sheriff3",
+  "sheriff2",
+  "sheriff",
+  "riot",
+  "fbi",
+  "hwaycar2",
+  "hwaycar3",
+  "hwaycar5",
+  "hwaycar6",
+  "hwaycar7",
+  "hwaycar8",
+  "hwaycar9",
+  "hwaycar10",
+  "hwaycar",
+  "polf430",
+  "policeb",
+  "RHINO"
+}
